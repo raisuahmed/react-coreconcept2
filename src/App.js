28c446db50c3ef51ff46a06e0dafbda011e16c52
raisuahmed <raisuahmed83@gmail.com>
2020-08-23 20:48:09 +0600

    recap

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const nayoks =['rasel','Kona']
   return (
     <div className="App">
+      <Nayok name = {nayoks [0]} food= "Pasta"></Nayok>
+       <Nayok name = {nayoks[1]}></Nayok>
+       <Nayok name = "Salman Shah"></Nayok>
+       <Nayok name = 'Sabnur'></Nayok>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} className="App-logo" alt ="logo" />
+       
       </header>
     </div>
   );
 }
 
+function Nayok(props){
+  console.log(props.name);
+  const NayokStyle = {
+    border:'2px solid red',
+    margin:'10px',
+    width:'300px',
+    backgroundColor:'grey'
+    
+  }
+  return (
+      <div style= {NayokStyle}>
+    <h1>Nayok:{props.name}</h1>
+  <h3>Food:{props.food}</h3>
+  </div>
+  )
+}
 export default App;
